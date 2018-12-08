c40c72778f6ddb8f3bccdd249244ae23df3c6508
Sandro Heinzelmann <sandro.heinzelmann@gmail.com>
2018-12-08 17:49:29 +0100

    First working attempt at integrating jq
    Only compiles on windows, no memory management or cleanup

--- README.md
+++ README.md
@@ -20,3 +20,30 @@ https://github.com/ashb/jqrepl
 
 c bindings in golang:  
 https://golang.org/cmd/cgo/
+
+### cross-compile jq for windows, on linux windows subsystem
+
+https://github.com/stedolan/jq/wiki/Cross-compilation
+
+```
+git clone https://github.com/stedolan/jq.git jq-master
+cd jq-master
+git submodule update --init
+```
+Note: 1.6 release doesn't work because of bug when compiling
+
+packages:
+
+```
+sudo apt-get install autoconf make libtool flex bison gcc-mingw-w64-x86-64
+```
+
+compiling:
+
+```
+autoreconf -fi
+./configure
+make distclean
+# Run it twice if first time you get fatal error: compile.h
+CPPFLAGS=-I$PWD/src scripts/crosscompile win64 --disable-shared --enable-static --enable-all-static --target=win64-x86_64 --host=x86_64-w64-mingw32 --with-oniguruma=builtin
+```
--- hello.go
+++ hello.go
@@ -1,9 +1,46 @@
 package main
 
+/*
+#cgo CFLAGS: -Ijq-master/build/win64/usr/local/include
+#cgo LDFLAGS: -Ljq-master/build/win64/usr/local/lib -ljq -lonig -lshlwapi
+#cgo linux LDFLAGS: -lm
+#include <jq.h>
+#include <jv.h>
+#include <stdlib.h>
+*/
+import "C"
 import "fmt"
 
 func main() {
-	fmt.Printf("hello, world\n")
+
+	state := C.jq_init()
+
+	// Compile jq program
+	prog := C.CString(".[] | select(.foo % 2 == 0) | .bar")
+	args := C.jv_array()
+	C.jq_compile_args(state, prog, args)
+
+	// Prepare input
+	flags := C.int(0)
+	input := `
+	[
+		{"foo": 7, "bar": "helloooo"},
+		{"foo": 8, "bar": "world"},
+		{"foo": 9, "bar": "wadup"},
+		{"foo": 10, "bar": "heheehe"}
+	]
+	`
+	jvInput := C.jv_parse(C.CString(input))
+
+	// Process input
+	dumpFlags := C.int(C.JV_PRINT_PRETTY | C.JV_PRINT_SPACE1)
+	C.jq_start(state, jvInput, flags)
+	res := C.jq_next(state)
+	for C.jv_is_valid(res) != 0 {
+		resStr := C.jv_dump_string(res, dumpFlags)
+		fmt.Printf("%s\n", C.GoString(C.jv_string_value(resStr)))
+		res = C.jq_next(state)
+	}
 }
 
 func foo(x int) int {
